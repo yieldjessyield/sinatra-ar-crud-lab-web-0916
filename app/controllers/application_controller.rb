09d814ab8ac452345c772d95e41e07f00c91ab49
Jess <jess.harrelson0707@gmail.com>
2016-10-16 00:01:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,60 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do
+    erb :'new'
+  end
+
+  get '/posts' do
+    @all_posts = Post.all
+    erb :index
+  end
+
+
+  get '/posts/:id' do
+    #binding.pry
+    @post_id = params[:id].to_i
+    @post = Post.find(@post_id)
+    erb :'show'
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+    #binding.pry
+    #redirect to('/posts/@post_id')
+  end
+
+
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+   post '/posts' do  #creates a post
+     @nothin = Post.create(name: params[:name], content: params[:content])
+     redirect to '/posts'
+     #binding.pry
+     #form in 'new' leads here with params
+     #create the blog post and save it to the database
+     #Post.create(some_attribute: params[:some_attribute])
+   end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @da_post = @post.name
+    @post.delete
+    erb :delete
+    #redirect to('/posts')
+  end
+
+
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,21 @@
-#Placeholder for a model
+class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+
+  # @@all = []
+
+  # def initialize(env)
+  #   @name = name
+  #   @content = content
+  #   @@all << self
+  # end
+
+  # def self.all
+  #   @@all
+  # end
+
+  # def self.clear
+  #   @@all.clear
+  # end
+
+end
